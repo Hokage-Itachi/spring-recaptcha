7491b56322856aba05448e32e2196ab74af29323
Itachi <annguyenvan.2k@gmail.com>
2021-07-06 15:33:04 +0700

    add annotation

---
+++ recaptcha-service/src/main/java/com/example/recaptchaservice/annotation/VerifyRecaptcha.java
@@ -0,0 +1,14 @@
+package com.example.recaptchaservice.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface VerifyRecaptcha {
+    String action();
+
+    float threshold() default 0f;
+}
